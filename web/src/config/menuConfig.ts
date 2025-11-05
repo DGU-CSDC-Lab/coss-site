08813582f5bdc217b2b529972f206b4085ffde07
EunJiJung <bianbbc87@gmail.com>
2025-11-05 14:04:11 +0900

    refactor: entity에서 corese master, offering 분리
    Signed-off-by: EunJiJung <bianbbc87@gmail.com>

--- web/src/config/menuConfig.ts
+++ web/src/config/menuConfig.ts
@@ -32,11 +32,11 @@ const menuConfig = [
         name: '개설과목',
         path: '/curriculum/subjects',
         children: [
-          { name: '학기별 개설 과목', path: '/curriculum/subjects/offered' },
-          {
+                    {
             name: '전체 운영 과목',
             path: '/curriculum/subjects/single-semester',
           },
+          { name: '학기별 개설 과목', path: '/curriculum/subjects/offered' }
         ],
       },
     ],
