fb059bd533e925a8435fd8453a90ff5499f93c52
EunJiJung <bianbbc87@gmail.com>
2025-11-11 05:34:15 +0900

    add: courses 추가 시 cource type, grade option 데이터 수정
    Signed-off-by: EunJiJung <bianbbc87@gmail.com>

--- web/src/config/courseConfig.ts
+++ web/src/config/courseConfig.ts
@@ -4,21 +4,18 @@ export const GRADE_OPTIONS = [
   { value: '', label: '수강학년 선택' },
   { value: '1학년', label: '1학년' },
   { value: '2학년', label: '2학년' },
+  { value: '1,2학년', label: '1,2학년' },
   { value: '3학년', label: '3학년' },
+  { value: '2,3학년', label: '2,3학년' },
   { value: '4학년', label: '4학년' },
-  { value: '대학원', label: '대학원' },
+  { value: '3,4학년', label: '3,4학년' },
   { value: '전체', label: '전체' },
 ]
 
 export const COURSE_TYPE_OPTIONS = [
   { value: '', label: '강의유형 선택' },
-  { value: '이론', label: '이론' },
-  { value: '실습', label: '실습' },
-  { value: '이론+실습', label: '이론+실습' },
-  { value: '세미나', label: '세미나' },
-  { value: '프로젝트', label: '프로젝트' },
-  { value: '현장실습', label: '현장실습' },
-  { value: '졸업논문', label: '졸업논문' },
+  { value: '전공', label: '전공' },
+  { value: '교양', label: '교양' },
 ]
 
 export const SEMESTER_OPTIONS = [
