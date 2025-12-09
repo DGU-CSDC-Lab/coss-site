36f1a3ac78f216ae1c35c582eb82f21b20276dc3
EunJiJung <bianbbc87@gmail.com>
2025-12-09 18:56:26 +0900

    refactor: build 시마다 이미지 파일에 대한 해시 코드를 추가해서 캐시로 인한 업데이트가 되지 않는 이슈 방지
    Signed-off-by: EunJiJung <bianbbc87@gmail.com>

--- web/vite.config.js
+++ web/vite.config.js
@@ -14,6 +14,18 @@ export default defineConfig(({ mode }) => ({
   },
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.')
+          const ext = info[info.length - 1]
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
+            return `assets/images/[name]-[hash][extname]`
+          }
+          return `assets/[name]-[hash][extname]`
+        },
+      },
+    },
   },
   envDir: '.',
   envPrefix: 'VITE_',
