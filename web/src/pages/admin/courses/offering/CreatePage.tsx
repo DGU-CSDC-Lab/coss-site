490e4b8bb8e5c5125d85560640e4c4815522995c
EunJiJung <bianbbc87@gmail.com>
2025-12-10 18:40:48 +0900

    refactor: 분반 드롭다운 01 잘리는 이슈 수정
    Signed-off-by: EunJiJung <bianbbc87@gmail.com>

--- web/src/pages/admin/courses/offering/CreatePage.tsx
+++ web/src/pages/admin/courses/offering/CreatePage.tsx
@@ -269,6 +269,7 @@ export default function AdminCourseOfferingCreatePage() {
                   <Dropdown
                     options={[
                       { value: '01', label: '01' },
+                      { value: '01', label: '01' },
                       { value: '02', label: '02' },
                       { value: '03', label: '03' },
                       { value: '04', label: '04' },
